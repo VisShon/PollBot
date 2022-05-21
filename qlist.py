d6f6db1de9358d257087d1dce8a68a7f12bf03d7
Vi\nU <88622653+VisShon@users.noreply.github.com>
2022-05-21 20:31:51 +0000

    yaya

--- qlist.py
+++ qlist.py
@@ -6,13 +6,13 @@ Ql=['What is axie infinity ?','How much Axie takes for buying and selling Axie N
 Al=['🔷','🔶','🟥','🟩','🟥','🔶','🟩','🟥','🔶']
 
 
-Q1l=['','4.25%','','','','+10%','','','By clicking the ❤ button']
+Q1l=['Space travel agency','4.25%','5','Saazy lime potion','Metamask','+10%','Decentralised-Authoritarian-Organisation','Beast Card against a Bird Axie has +10% attack penalty','By clicking the ❤ button']
 
 
-Q2l=['','','3','','Ronin','','','Beast Card against a Bird Axie has -10% attack penalty','']
+Q2l=['A gaming guild','3.25%','3','Super Limited Point','Ronin','-10%','Digital-Auto-Organiser','Beast Card against a Bird Axie has -10% attack penalty','By going to hospital']
 
 
-Q3l=['Play-To-Earn cryptocurrency game','','','','','','','','']
+Q3l=['Play to Earn cryptocurrency game','15%','1','Smart Log Potion','Zengo','+20%','Dangerous-Autonomous-Organism','Beast Card against a Bird Axie has -30% attack penalty','By not buying condoms']
 
 
-Q4l=['','','','Smooth Love Potion','','','Decentralised-Autonomous-Organisation','','']
+Q4l=['Bird feeding simulator','50%','0','Smooth Love Potion','Kraken','-20%','Decentralised-Autonomous-Organisation','Beast Card against a Bird Axie has 0% attack penalty','Through a Special NFT']
