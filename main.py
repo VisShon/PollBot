6f2ceed0bdd8d68b141e4dd2657b687763e87193
VisShon <vishnu20414@iiitd.ac.in>
2022-05-28 01:26:42 +0530

    updates

--- main.py
+++ main.py
@@ -3,28 +3,38 @@ import discord
 from discord.ext import commands
 import question_embed
 import pymongo
-# import qlist
-# import asyncio
-# import time
-# TOKEN = os.getenv('token')
 
+# list of emote options
 optList = ['🔶','🟥','🔷','🟩']
 
+# connecting to bot
 bot = commands.Bot(command_prefix='!')
 client = discord.Client()
 
+# connecting to mongodb
 Mclient = pymongo.MongoClient("mongodb://localhost:27017/")
 db = Mclient["respct"]
 
+@bot.command(name="quiz", help="Redirect to the quiz page")
+async def quiz(ctx):
+  embed = discord.Embed(
+        title='Respct OnBoarding Application',
+        description=
+        'To apply for a membership to our platform, \nDm me and take the quiz using !membership command to \nverify yourself',
+        color=0xA020F0)
+  embed.set_image(url="https://i.ibb.co/HqmsfNv/Mask-Group.png")
+  await ctx.send(embed=embed)
 
-@bot.command()
+
+@bot.command(name="respct", help="start the quiz")
 async def respct(ctx):
+    # generating the initial embed
     embed = discord.Embed(
         title='Respct OnBoarding Application',
         description=
-        'Congratulations! You are now a verified \nmember of our Discord.\n\n\nIf you want to become a member of Respct,\ngo ahead, read our white paper (link) and then take a short quiz!\n\n\nClick on the green button to start the quiz and get onboard.',
+        'Congratulations! You are now a verified \nmember of our Discord.\n\n\nIf you want to become a member of Respct,\ngo ahead, read our white paper (link) and then take a short quiz!\n\n\nClick on the green button to start the quiz and get onboard.\n\n',
         color=0xA020F0)
-    embed.set_image(url="https://www.respct.fun/Respct_d.png")
+    embed.set_image(url="https://i.ibb.co/qjgb7Z5/Group-48.png")
     buttons = discord.ui.Button(label="Start",
                                 style=discord.ButtonStyle.green,
                                 emoji='⚡')
@@ -33,54 +43,88 @@ async def respct(ctx):
     view.add_item(buttons)
     await ctx.send(embed=embed, view=view)
 
+@bot.command(name="status", help="Give the user status")
+async def status(ctx):
+  col = db["Users"]
+  query={"DiscordId":"Vis.n_u#3089"}
+  data =  col.find(query)
+  for i in data:
+    x=
+  embed = discord.Embed(
+        title='Respct OnBoarding Application',
+        description=
+        '',
+        color=0xA020F0)
+  embed.set_image(url="https://i.ibb.co/HqmsfNv/Mask-Group.png")
+  await ctx.send(embed=embed)
 
 @bot.event
-async def on_button_click(interaction, button):
-    user = interaction.user.
+async def on_button_click(interaction,button):
+
+    user = str(interaction.user)
+    channel = interaction.channel
+
+    embed = discord.Embed(
+          title='Respct OnBoarding Application',
+          description=
+          'Enter GuildName / GameName\nyou want to give the quiz of\n\nExample: IndieGG/AxieInfinity',
+          color=0xA020F0)
+    embed.set_image(url="https://cdn3d.iconscout.com/3d/premium/thumb/game-controller-4035922-3342601@0.png")
+    await interaction.response.send_message(embed=embed)
+
+
+    def check(m):
+        return True
+    msg = await bot.wait_for("message", check=check)
+    arr = msg.content.split("/")
+    guildName="IndieGG"
+    gameName="AxieInfinity"
+
+
     embed = discord.Embed(
           title='Respct OnBoarding Application',
           description=
           'Quiz Started',
           color=0xA020F0)
-
-  
-    await interaction.response.send_message(embed=embed)
+    await interaction.followup.send(embed=embed)
   
     pts = 0
     n=0
-    channel = interaction.channel
-    message = interaction.message
-
-    col = db["Quizes"]
-    query={"guildName":"IndieGG"}
-    data =  col.find(query)
 
     while (pts != 9):
-  
-      
-      for ques in data:
-        Q=ques["games"]["AxieInfinity"][pts]["Q"]
-        op1=ques["games"]["AxieInfinity"][pts]["op1"]
-        op2=ques["games"]["AxieInfinity"][pts]["op2"]
-        op3=ques["games"]["AxieInfinity"][pts]["op3"]
-        op4=ques["games"]["AxieInfinity"][pts]["op4"]
-      embed = question_embed.qe(pts+1,Q,op1,op2,op3,op4)
 
+      # fetching the quiz collection
+      col = db["Quizes"]
+      query={"guildName":guildName}
+      data =  col.find(query)
+  
+      for ques in data:
+        Q=ques["games"][gameName][pts]["Q"]
+        op1=ques["games"][gameName][pts]["op1"]
+        op2=ques["games"][gameName][pts]["op2"]
+        op3=ques["games"][gameName][pts]["op3"]
+        op4=ques["games"][gameName][pts]["op4"]
+
+      embed = question_embed.qe(pts+1,Q,op1,op2,op3,op4)
       returnVal = await channel.send(embed=embed)
 
+      # generating options from
       for e in optList:
         await returnVal.add_reaction(e)
 
       answered = False
       playerInput = None
 
+      # listner for response
       while not answered:
         returnVal = await returnVal.edit(embed=embed)
         for x in returnVal.reactions:
           if x.count == 2:
             answered = True
             playerInput = x
-      if str(playerInput.emoji) == ques["games"]["AxieInfinity"][pts]["ans"]:
+
+      # check for correct answer
+      if str(playerInput.emoji) == ques["games"][gameName][pts]["ans"]:
         n+=1
       pts += 1
 
@@ -96,14 +140,13 @@ async def on_button_click(interaction, button):
         color=0xA020F0)
 
     col = db["Users"] 
-    data={"DiscordId":user,"guild":"IndieGG","game":"AxieInfinity","score":n}
+    data={"DiscordId":user,"guild":guildName,"game":gameName,"score":n}
     col.insert_one(data)
 
     if(n>4):
       await channel.send(embed=endEmbedPass)
     else:
       await channel.send(embed=endEmbedFail)
-
 
 
 bot.run('OTc2OTQ3NzcwMjE4OTMwMTk2.G1puY2.X6GYkajOvC562QkA0b-FXbUuNR7NMdhKi7-mi8')
